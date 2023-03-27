14d0129afce3741cfcc040f72cd58ee424b566b6
Murthy <murthychiluka@gmail.com>
2023-03-27 18:12:52 +0530

    d

--- vars.tf
+++ vars.tf
@@ -10,3 +10,7 @@ variable "subnets" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
 
+# locals {
+#   dns_word = var.env == "prod" ? "www" : var.env
+#   dns_name = var.component == "frontend" ? "${local.dns_word}.${var.dns_domain}" : "${var.component}-${var.env}.${var.dns_domain}"
+# }
